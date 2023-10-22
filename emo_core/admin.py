427db6760c5fc7b2889ddc38b326556dd049ffea
yatoyun <kaneko.yuta0813@gmail.com>
2023-10-22 20:53:00 +0900

    :zap: update admin browse

--- emo_core/admin.py
+++ emo_core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserModel, EmotionData, ChatLogs, AdviceData
 
-# Register your models here.
+admin.site.register(UserModel)
+admin.site.register(EmotionData)
+admin.site.register(ChatLogs)
+admin.site.register(AdviceData)
